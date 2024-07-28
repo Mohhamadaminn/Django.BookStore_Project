5f77d78692cb776a94c48cde2448fca506675a0e
Mohamadamin <moaminalizadeh21@gmail.com>
2024-07-28 14:57:58 +0330

    add update and delete views

--- books/urls.py
+++ books/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.BooksListView.as_view(), name='book_list'),
     path('<int:pk>/', views.BooksDetailView.as_view(), name='book_detail'),
-
+    path('new/', views.BooksCreateView.as_view(), name='book_create'),
+    path('<int:pk>/edit/', views.BookUpdateView.as_view(), name='book_update'),
+    path('<int:pk>/delete/', views.BookDeleteView.as_view(), name='book_delete'),
 ]
