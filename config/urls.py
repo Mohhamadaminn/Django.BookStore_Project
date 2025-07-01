3ec1f5a930db071893008accef48748c76111ecc
Mohhamadaminn <mohamad4aminm@gmail.com>
2025-07-01 19:50:01 +0330

    start creating cart feature

--- config/urls.py
+++ config/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('books/', include('books.urls')),
+    path('cart/', include('cart.urls')),
     path('', include('pages.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('accounts.urls')),
